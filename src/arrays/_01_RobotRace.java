21c9a5ebd9256202b825ff7c7642823b938bc210
HeyItzGinger <league@iMac-2.attlocal.net>
2018-07-23 16:58:15 -0700

    Robot Race (ip)

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,22 +1,38 @@
 package arrays;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
-
+public static void main(String[] args) {
+	
 	//2. create an array of 5 robots.
-
+	Robot[] iRobs = new Robot[5];
 	//3. use a for loop to initialize the robots.
-
+	int posX = 100;
+	for(int m = 0; m < iRobs.length; m++) {
+		iRobs[m] = new Robot(posX, 450);
+		iRobs[m].setSpeed(25);
+	iRobs[m].miniaturize();
 		//4. make each robot start at the bottom of the screen, side by side, facing up
-
+		posX += 100;
+	}
 	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
-
+	//   a random amount less than 50
+	Random ran = new Random();
+	int r = ran.nextInt(50);
+	for(int j = 0; j < iRobs.length; j++) {
+		iRobs[j].move(r);
+	}
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
+	
 	//7. declare that robot the winner and throw it a party!
 
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+	
+}
 }
