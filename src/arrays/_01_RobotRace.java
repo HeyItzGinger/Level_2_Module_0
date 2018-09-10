abbd248a0f05cfab1061b8d034c366d0eed12b4e
HeyItzGinger <league@iMac-2.attlocal.net>
2018-09-10 16:59:01 -0700

    Robot Race, Hidden Button (ip)

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -22,21 +22,22 @@ public class _01_RobotRace {
 		// 5. use another for loop to iterate through the array and make each robot move
 		// a random amount less than 50
 		Random ran = new Random();
-		int r = ran.nextInt(50);
 		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
 		// screen.
 		int smallestY = 1000;
 		int winner = 0;
 		while (smallestY > 0) {
 			for (int j = 0; j < iRobs.length; j++) {
+				int r = ran.nextInt(50);
 				iRobs[j].move(r);
 				if (iRobs[j].getY() < smallestY) {
 					smallestY = iRobs[j].getY();
+					winner=j;
 				}
 			}
 		}
 		// 7. declare that robot the winner and throw it a party!
-
+			System.out.println("Robot number: " + winner + " won!!!!!!!!");
 		// 8. try different races with different amounts of robots.
 
 		// 9. make the robots race around a circular track.
--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -20,14 +20,17 @@ public class _02_FindTheHiddenButton implements ActionListener{
 	JPanel panel;
 	
 	//1. create an array of JButtons. Don't initialize it yet.
-	
 	//2 create an int variable called hiddenButton
-	
+	int hiddenButton;
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
 	
 	public void start() {
+		String j = JOptionPane.showInputDialog("How many buttons do you want?");
+		int jInt = Integer.parseInt(j);
+		JButton [] buttonList = new JButton[jInt];
+
 		window = new JFrame("Find the Button");
 		panel = new JPanel();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -35,7 +38,9 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		//3.  Ask the user to enter a positive number and convert it to an int
 		
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		
+		for(int i = 0; i < buttonList.length; i ++) {
+			
+		}
 		//5. Make a for loop to iterate through the JButton array
 			//6. initialize each JButton in the array
 			//7. add the ActionListener to each JButton
